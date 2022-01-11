f943f2e563852c065233e66e920e86a4b1331579
AmauriLima <amauri.plimaj@gmail.com>
2022-01-11 11:18:30 -0300

    feat: adding page home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from 'next/head';
 
+import Avatar from '../assets/images/avatar.svg';
+import { Container, Hero } from '../styles/home';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello world</h1>
+      <Container>
+        <Hero>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acces to all the publication <br />
+            <span>for $9.90 month</span>
+          </p>
+        </Hero>
+        <Avatar />
+
+      </Container>
     </>
   );
 }
